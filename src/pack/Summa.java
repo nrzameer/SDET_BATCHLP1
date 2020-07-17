14f709b4faab4824c77ddb29e0cc7b9be1ccb90a
nrzameer <nrzameer@gmail.com>
2020-07-17 10:14:53 +0530

    sample commit

---
+++ src/pack/Summa.java
@@ -0,0 +1,26 @@
+package pack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Summa {
+	
+	
+	public void ex() {
+System.out.println("test");
+	}
+	
+
+	public static void main(String[] args) {
+
+		Summa s = new Summa();
+		s.ex();
+	
+		
+		
+		
+		
+	}
+
+}
